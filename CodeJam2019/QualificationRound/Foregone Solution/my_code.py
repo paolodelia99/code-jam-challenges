681bcb9c93928df590e990a1ab817bea19386647
Paolo D'Elia <paolo.delia99@gmail.com>
2020-03-16 14:21:30 +0100

    Solved Foregone solution

--- CodeJam2019/QualificationRound/Foregone Solution/my_code.py
+++ CodeJam2019/QualificationRound/Foregone Solution/my_code.py
@@ -1,18 +1,17 @@
-from random import randint
-import re
-
 t = int(input())
 
-for _ in range(1,t+1):
-    n = int(input())
-    repeatCicle = True
-    firstNumber, randnum = 0,0
+for _ in range(1, t + 1):
+    n = input()
+    # reverse the string number
+    n = ''.join(reversed(n))
+    firstNum, secondNum = 0, 0
 
-    while repeatCicle:
-        randnum = randint(1,n/2)
-        firstNumber = n - randnum
-        randnum_check = re.match(r"*4*", str(randnum))
-        repeatCicle = True if '4' in str(randnum) and '4' in str(firstNumber) else False
+    for i in range(0,len(n)):
+        # if n[i] is 4 I divide it into 2 and 2
+        if n[i] == '4':
+            firstNum += 2*10**i
+            secondNum += 2*10**i
+        else:
+            firstNum += int(n[i])*10**i
 
-
-    print("Case #{}: {} {}".format(_, firstNumber,randnum))
+    print("Case #{}: {} {}".format(_, firstNum, secondNum))
