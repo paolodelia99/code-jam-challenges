77c500a732b815cd7c20808017ce0714c09c15b5
Paolo D'Elia <paolo.delia99@gmail.com>
2020-03-27 15:42:37 +0100

    Starting Edgy Barking

--- CodeJam2018/Round1A/Bit Party/sol.py
+++ CodeJam2018/Round1A/Bit Party/sol.py
@@ -1,17 +1,35 @@
 t = int(input())
 
-for case in range(1,t+1):
-    r,b,c = map(int, input().split())
-    cashier = []
+
+def check(t, r, b, m, s, p):
+    c = [0] * len(m)
+
+    for i in range(len(c)):
+        c[i] = max(0, min(m[i], (t - p[i]) // s[i]))
+
+    c.sort(reverse=True)
+
+    return sum(c[:r]) >= b
+
+
+def bit_party():
+    r, b, c = map(int, input().split())
+    m, s, p = [0] * c, [0] * c, [0] * c
 
     for i in range(c):
-        cashier.append(tuple(map(int, input().split())))
+        m[i], s[i], p[i] = map(int, input().strip().split())
+
+    left, right = 0, max(s) * b + max(p)
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        if check(mid, r, b, m, s, p):
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return left
 
 
-    max_bit = b / r if b % r == 0 else (b % r) + 1
-
-    time_for_cash = []
-
-    for i in cashier:
-        if i[0] >= max_bit:
-            time_for_cash.append((max_bit * i[1]) + i[2])
+for case in range(1, t + 1):
+    print('Case #{}: {}'.format(case, bit_party()))
---
+++ CodeJam2018/Round1A/Edgy Barking/my_code.py
@@ -0,0 +1,5 @@
+"""
+    ogni rettangolo ha 3 possibili tagli,
+    loopare tra i rettangoli fino a che non si è ragginunta la somma
+    richiesta(o si è vicini alla somma richiesta)
+"""
