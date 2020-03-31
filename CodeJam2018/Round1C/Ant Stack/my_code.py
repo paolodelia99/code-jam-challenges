ec9a81483724e479b6fe13008c6a843443ccbfeb
unknown <paolo.delia99@gmail.com>
2020-03-31 14:10:35 +0200

    Still stuck in ant stack

--- CodeJam2018/Round1C/Ant Stack/my_code.py
+++ CodeJam2018/Round1C/Ant Stack/my_code.py
@@ -2,19 +2,19 @@ t = int(input())
 
 
 def countAnts(ants, x, y):
-    if x == 0:
-        return 1
+    if x >= len(ants):
+        return 0
     else:
-        if sum(ants[0:x]) <= y:
-            return 1 + countAnts(ants, x - 1, ants[x]*6)
+        if ants[x]*6 >= y:
+            return 1 + countAnts(ants, x + 1, y+ants[x])
         else:
-            return countAnts(ants, x - 1, y)
+            return countAnts(ants, x + 1 , y)
 
 
 for case in range(1, t + 1):
     n = int(input())
     ants = list(map(int, input().split()))
 
-    res = countAnts(ants , len(ants)-1 , ants[len(ants)-1]*6)
+    res = countAnts(ants , 0 , 0)
 
     print("Case #{}: {}".format(case, res))
